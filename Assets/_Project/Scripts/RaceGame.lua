2e3749f5fa432501bdff13c5a8c98746b4118863
FarhanGul <FarhanGulKhan29@gmail.com>
2024-04-26 23:16:39 +0500

    Board and dice state replicated on all clients

--- Assets/_Project/Scripts/RaceGame.lua
+++ Assets/_Project/Scripts/RaceGame.lua
@@ -11,39 +11,48 @@ local playing = 1
 --
 local gameState = waitingForPlayers
 --Events
-local fetchGameStateFromServer = Event.new("fetchGameStateFromServer")
-local sendGameStateToClient = Event.new("sendGameStateToClient")
+local e_fetchGameStateFromServer = Event.new("fetchGameStateFromServer")
+local e_sendGameStateToClient = Event.new("sendGameStateToClient")
+local e_sendRollToClient = Event.new("sendRollToClient")
+local e_sendRollToServer = Event.new("sendRollToServer")
 --
 local players = {}
 
 
 function self:ClientAwake()
     board = boardGameObject:GetComponent("Board")
-    diceTapHandler.gameObject:GetComponent(TapHandler).Tapped:Connect(function() 
-        board.Move(math.random(1,6))
+    diceTapHandler.gameObject:GetComponent(TapHandler).Tapped:Connect(function()
+        e_sendRollToServer:FireServer(math.random(1,6)) 
     end)
-    sendGameStateToClient:Connect(function(newPlayers)
+    e_sendGameStateToClient:Connect(function(newPlayers)
         players = newPlayers
         HandlePlayersUpdated()
     end)
-    fetchGameStateFromServer:FireServer()
+    e_sendRollToClient:Connect(function(roll)
+        board.Move(roll)
+    end)
+    e_fetchGameStateFromServer:FireServer()
 end
 
 function self:ServerAwake()
     server.PlayerConnected:Connect(function(player)
         print(player.name .. " connected to the world on this client")
         table.insert(players,player)
-        sendGameStateToClient:FireAllClients(players)
+        e_sendGameStateToClient:FireAllClients(players)
     end)
 
     server.PlayerDisconnected:Connect(function(player)
         print(player.name .. " disconnected from the world on this client")
         table.remove(players,table.find(players, player))
-        sendGameStateToClient:FireAllClients(players)
+        e_sendGameStateToClient:FireAllClients(players)
     end)
 
-    fetchGameStateFromServer:Connect(function(player)
-        sendGameStateToClient:FireAllClients(players)
+    e_fetchGameStateFromServer:Connect(function(player)
+        e_sendGameStateToClient:FireAllClients(players)
+    end)
+
+    e_sendRollToServer:Connect(function(player, roll)
+        e_sendRollToClient:FireAllClients(roll)
     end)
 end
 
