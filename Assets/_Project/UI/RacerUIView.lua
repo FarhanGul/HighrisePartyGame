dcca584aa1c1620c71d6cef3f81cf7e8391bd942
FarhanGul <FarhanGulKhan29@gmail.com>
2024-04-29 15:24:01 +0500

    Added color to text

--- Assets/_Project/UI/RacerUIView.lua
+++ Assets/_Project/UI/RacerUIView.lua
@@ -4,12 +4,12 @@
 local usernames: VisualElement = nil
 
 function SetPlayer(id,player)
-    local usernameBlock = usernames:Q("player_"..id)
+    local usernameBlock = usernames:Q("userblock_"..id)
     if (player == nil) then
         usernameBlock.visible = false
         return
     end
     usernameBlock.visible = true
-    usernameBlock:Q("l_"..id):SetPrelocalizedText(player.player.name, false)
-    usernameBlock:Q("t_"..id).visible = player.isTurn
+    usernameBlock:Q("username_"..id):SetPrelocalizedText(player.player.name, false)
+    usernameBlock:Q("turn_indicator_"..id).visible = player.isTurn
 end
