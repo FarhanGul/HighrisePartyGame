180dc14471e73021ba5c16709af122d4a2630134
FarhanGul <FarhanGulKhan29@gmail.com>
2024-05-24 20:26:25 +0500

    Added color support to generic text

--- Assets/_Project/UI/GenericText.lua
+++ Assets/_Project/UI/GenericText.lua
@@ -8,6 +8,10 @@ local isSmall: boolean = false
 local isTiny: boolean = false
 --!SerializeField
 local label: string = ""
+--!SerializeField
+local applyCustomColor: boolean = false
+--!SerializeField
+local customColor: Color = Color.clear
 
 --!Bind
 local generic_text: UILabel = nil
@@ -24,6 +28,11 @@ function SetText(text)
 end
 
 function UpdateStyling()
+    if(applyCustomColor) then
+        generic_text.style.color = StyleColor.new(customColor)
+    else
+        generic_text.style.color = StyleColor.new(Color.white)
+    end
     generic_text:EnableInClassList("heading_text",isHeading)
     generic_text:EnableInClassList("normal_text",not isHeading and not isSmall and not isTiny)
     generic_text:EnableInClassList("small_text",isSmall)
