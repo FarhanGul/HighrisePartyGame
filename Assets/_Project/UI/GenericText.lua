278612fe2074b0dcb8d6fe18f9fbebcfcb4e082d
FarhanGul <FarhanGulKhan29@gmail.com>
2024-05-24 19:32:41 +0500

    Added labels to hud

--- Assets/_Project/UI/GenericText.lua
+++ Assets/_Project/UI/GenericText.lua
@@ -2,16 +2,27 @@
 
 --!SerializeField
 local isHeading: boolean = false
+--!SerializeField
+local isSmall: boolean = false
+--!SerializeField
+local label: string = ""
 
 --!Bind
 local generic_text: UILabel = nil
 
 function self:ClientAwake()
     -- SetText("FarhanGulDev Played AntimatterCannon but was blocked")
+    if(label ~= "") then SetText(label) end
+    UpdateStyling()
 end
 
 function SetText(text)
-    generic_text:EnableInClassList("heading_text",isHeading)
-    generic_text:EnableInClassList("normal_text",not isHeading)
+    UpdateStyling()
     generic_text:SetPrelocalizedText(text, false)
+end
+
+function UpdateStyling()
+    generic_text:EnableInClassList("heading_text",isHeading)
+    generic_text:EnableInClassList("normal_text",not isHeading and not isSmall)
+    generic_text:EnableInClassList("small_text",isSmall)
 end
